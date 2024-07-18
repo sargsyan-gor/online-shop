ab2b8588b3be45c99c2f4cb410bbc5253e4f58a7
Gor <gor.sargsyaann@gmail.com>
2024-07-18 14:44:41 +0400

    ran seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,19 +5,25 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
+        $faker = Faker::create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        foreach (range(1, 20) as $index) {
+            DB::table('posts')->insert([
+                'user_id' => $faker->numberBetween(1,10),
+                'title' => $faker->name,
+                'content' => $faker->unique()->safeEmail,
+                'price' => $faker->numberBetween(10,20),
+            ]);
+        }
     }
 }
